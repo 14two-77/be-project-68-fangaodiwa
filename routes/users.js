47087e3d767417c5e4d16506ffb401479d22ee13
Thanachart Sangmola <thanachart.farn@gmail.com>
2026-03-24 18:52:04 +0700

    add user apis

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,18 @@
 const express = require('express');
-const { updateUserTier } = require('../controllers/users');
+const { getUsers, getUser, updateUser, deleteUser, updateUserTier } = require('../controllers/users');
 const { protect, authorize } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.use(protect);
+
+router.route('/')
+  .get(authorize('admin'), getUsers);
+
+router.route('/:id')
+  .get(authorize('admin'), getUser)
+  .put(authorize('admin'), updateUser)
+  .delete(authorize('admin'), deleteUser);
 
 router.route('/:id/tier')
   .put(authorize('admin'), updateUserTier);
